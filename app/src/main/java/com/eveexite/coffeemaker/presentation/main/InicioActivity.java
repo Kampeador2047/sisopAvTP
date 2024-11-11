2a07b9bd793f01a3550ecfe535b2d6abcd4b0b5e
Milos <milos2002@hotmail.com.ar>
2024-11-11 20:42:48 -0300

    agregado de shake para boton de encendido

--- app/src/main/java/com/eveexite/coffeemaker/presentation/main/InicioActivity.java
+++ app/src/main/java/com/eveexite/coffeemaker/presentation/main/InicioActivity.java
@@ -12,10 +12,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.eveexite.coffeemaker.R;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class InicioActivity extends AppCompatActivity implements ISingletonActivities {
+
+public class InicioActivity extends AppCompatActivity implements ISingletonActivities, SensorEventListener{
 
     private MqttHandler mqttHandler;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private static final float SHAKE_THRESHOLD = 18.0f;
+    private static final long INTERVAL_SHAKE= 1000;
+    private long lastShakeTime = 0;
+    private boolean shakeDetected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,12 @@ public class InicioActivity extends AppCompatActivity implements ISingletonActiv
         initializeView();
         mqttHandler = new MqttHandler(getApplicationContext());
         connect();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
 
     }
 
@@ -100,4 +117,35 @@ public class InicioActivity extends AppCompatActivity implements ISingletonActiv
         }
 
     }
+
+    // Métodos de SensorEventListener
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER ) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float acceleration = (float) Math.sqrt(x * x + y * y + z * z) - SensorManager.GRAVITY_EARTH;
+            long currentTime = System.currentTimeMillis();
+
+            if (!shakeDetected && acceleration > SHAKE_THRESHOLD && (currentTime - lastShakeTime) > INTERVAL_SHAKE) {
+                shakeDetected = true;
+                lastShakeTime = currentTime;
+                publishMessage(MqttHandler.TOPIC_POWER, "1");
+                Toast.makeText(this, "Shake detectado! Encendiendo máquina.", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(InicioActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 }
